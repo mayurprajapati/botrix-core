6e9b5b851841f2fbf89c4b589ee5ee07e08aff56
Mayur Prajapati <mayur@humbingo.com>
2024-05-03 10:10:11 +0530

    Add recaptcha support

--- src/main/java/appium/wrapper/driver/AppiumDriverWrapper.java
+++ src/main/java/appium/wrapper/driver/AppiumDriverWrapper.java
@@ -1,15 +1,22 @@
 package appium.wrapper.driver;
 
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 import java.util.Set;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.devtools.Connection;
+import org.openqa.selenium.devtools.HasDevTools;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import appium.wrapper.locator.AppiumLocator;
+import grpc.bridge.python.PythonBridgeClient;
 
 public class AppiumDriverWrapper {
 	RemoteWebDriver driver;
@@ -39,9 +46,27 @@ public class AppiumDriverWrapper {
 
 	public Optional<AppiumWebElement> findOneIfPresent(AppiumLocator loc) {
 		try {
-			return Optional.of(findOne(loc));
-		} catch (NoSuchElementException e) {
+			return Optional.of(findAll(loc).get(0));
+		} catch (NoSuchElementException | IndexOutOfBoundsException e) {
 			return Optional.empty();
+		}
+	}
+
+	public void resolveRecaptcha() {
+		URI url = getDevtoolsUri();
+		PythonBridgeClient.resolveRecaptcha(url.getHost(), url.getPort());
+	}
+
+	public URI getDevtoolsUri() {
+		try {
+			Field connectionField = FieldUtils.getField(org.openqa.selenium.devtools.DevTools.class, "connection",
+					true);
+			Field urlField = FieldUtils.getField(org.openqa.selenium.devtools.Connection.class, "url", true);
+			Connection connection = (Connection) connectionField.get(((HasDevTools) driver).getDevTools());
+			URI url = new URI((String) urlField.get(connection));
+			return url;
+		} catch (IllegalAccessException | IllegalArgumentException | URISyntaxException e) {
+			throw new RuntimeException("Unable to get Browser Devtools URI", e);
 		}
 	}
 
@@ -55,6 +80,10 @@ public class AppiumDriverWrapper {
 
 	public Set<Cookie> getCookies() {
 		return driver.manage().getCookies();
+	}
+
+	public boolean isRecaptchaAvailable() {
+		return isPresent(AppiumLocator.byXpath("reCAPTCHA", "//iframe[@title=\"reCAPTCHA\"]"));
 	}
 
 	public void quit() {
--- src/main/java/grpc/bridge/python/PythonBridgeClient.java
+++ src/main/java/grpc/bridge/python/PythonBridgeClient.java
@@ -4,13 +4,9 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class PythonBridgeClient {
-	private ManagedChannel channel = null;
+	private static ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052).usePlaintext().build();
 
-	public PythonBridgeClient() {
-		channel = ManagedChannelBuilder.forAddress("localhost", 50052).usePlaintext().build();
-	}
-
-	public void resolveRecaptcha(String ip, int port) {
+	public static void resolveRecaptcha(String ip, int port) {
 		var stub = PythonBridgeGrpc.newBlockingStub(channel);
 		stub.resolveRecaptcha(DriverDebugAddressRequest.newBuilder().setIp(ip).setPort(port).build());
 	}
