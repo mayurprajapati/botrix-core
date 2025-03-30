0acc2a7e03f1a42a8e4f0597e9cc3b593a57f5fe
Mayur Prajapati <mayur@humbingo.com>
2025-03-30 16:37:34 +0530

    logger

--- src/main/java/botrix/utils/HasLogger.java
+++ src/main/java/botrix/utils/HasLogger.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public interface HasLogger {
-	public final Logger logger = LoggerFactory.getLogger(getCallerClassName());
+	public final Logger LOGGER = LoggerFactory.getLogger(getCallerClassName());
 
 	private static String getCallerClassName() {
 		return Thread.currentThread().getStackTrace()[3].getClassName();
